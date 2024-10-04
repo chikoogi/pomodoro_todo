f09f3fabb8e5ffbf0d79758daa3915c404359718
chikoogi <ehthfdhkd@gmail.com>
2024-10-04 19:23:05 +0900

    [New feature] test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -23,3 +23,9 @@ document.addEventListener("DOMContentLoaded", async () => {
   // 할 일 목록 렌더링
   renderTodoLists(todoLists);
 });
+
+// 상태가 변경될 때 호출하여 로컬 스토리지에 저장하는 함수
+export function updateTodoLists(updatedTodoLists) {
+  localStorage.setItem("todoLists", JSON.stringify(updatedTodoLists));
+  setState(updatedTodoLists); // 상태 변경 후 렌더링
+}
