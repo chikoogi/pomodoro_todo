209ddf6019d3ada89df13609fbcb00b2148431c8
chikoogi <ehthfdhkd@gmail.com>
2024-10-08 23:00:17 +0900

    [New feature] 타입스크립트 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,10 +10,10 @@ module.exports = {
     "plugin:prettier/recommended"
   ],
   parserOptions: {
-    ecmaVersion: 12, // 최신 ECMAScript 버전
-    sourceType: "module", // ES6 모듈 사용
+    ecmaVersion: 12,
+    sourceType: "module",
   },
-  ignorePatterns: ["dist/"], // dist 폴더 무시
+  ignorePatterns: ["dist/"],
   plugins: ["@typescript-eslint", "prettier"],
   overrides: [
     {
