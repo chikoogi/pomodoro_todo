1bef8092db11c9803f301fe74ac6a01a80bba884
chikoogi <ehthfdhkd@gmail.com>
2024-10-08 23:11:38 +0900

    [New feature] 버그 수정

--- src/components/Timer.ts
+++ src/components/Timer.ts
@@ -36,8 +36,8 @@ export class Timer {
   }
 
   setRemainingTime(minTime) {
-    // this.remainingTime = minTime * 60;
-    this.remainingTime = 3;
+    this.remainingTime = minTime * 60;
+    // this.remainingTime = 3;
   }
 
   is() {
