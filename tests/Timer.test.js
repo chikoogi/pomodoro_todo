b7e651f38c220ff9e6dd3bed2f7da8cc51faf4a5
chikoogi <ehthfdhkd@gmail.com>
2024-10-09 13:31:33 +0900

    [New feature] 테스트 수정

--- tests/Timer.test.js
+++ tests/Timer.test.js
@@ -1,7 +1,7 @@
 import { afterEach, beforeEach, describe, expect, jest, test } from "@jest/globals";
 import { Timer } from "../src/components/Timer.ts";
 
-describe("Timer", () => {
+describe("Timer 테스트", () => {
   let timer, initState;
 
   beforeEach(() => {
@@ -20,7 +20,7 @@ describe("Timer", () => {
     jest.useRealTimers();
   });
 
-  test("타이머 시작", () => {
+  test("타이머 시작 후 완료함수가 실행되는지 확인", () => {
     const onIncreaseCount = jest.fn();
     timer.setRemainingTime(1);
     timer.start(onIncreaseCount);
@@ -31,7 +31,7 @@ describe("Timer", () => {
     jest.advanceTimersByTime(60 * 1000 + 1000);
     expect(onIncreaseCount).toHaveBeenCalled();
   });
-  test("타이머 종료", () => {
+  test("타이머 정지 후 타이머가 초기화 되는지 확인", () => {
     const onIncreaseCount = jest.fn();
     const clearIntervalSpy = jest.spyOn(global, "clearInterval");
     timer.setRemainingTime(1);
@@ -48,7 +48,7 @@ describe("Timer", () => {
     expect(timer.activeItem).toBeNull();
     expect(timer.el.textContent).toBe(initState.elTextContent);
   });
-  test("타이머 일시정지 및 재시작", () => {
+  test("타이머 일시정지하면 시간이 정지하고 재시작하면 다시 일시정지한 시간부터 시작되는지 확인", () => {
     const onIncreaseCount = jest.fn();
     const clearIntervalSpy = jest.spyOn(global, "clearInterval");
     const setIntervalSpy = jest.spyOn(global, "setInterval");
@@ -56,17 +56,17 @@ describe("Timer", () => {
     timer.setRemainingTime(1);
     timer.start(onIncreaseCount);
 
-    // 타이머가 실행되고 30초 후를 시뮬레이션
+    /*타이머가 실행되고 30초 후를 시뮬레이션*/
     jest.advanceTimersByTime(30 * 1000);
 
     timer.pause();
     expect(clearIntervalSpy).toHaveBeenCalled();
 
-    // 타이머가 중지된 상태인지 확인
+    /*타이머가 중지된 상태인지 확인*/
     expect(timer.remainingTime).toBe(0.5 * 60);
     expect(onIncreaseCount).not.toHaveBeenCalled();
 
-    // 중지된 후에도 시간이 더 이상 줄어들지 않는지 확인
+    /*중지된 후에도 시간이 더 이상 줄어들지 않는지 확인*/
     jest.advanceTimersByTime(30 * 1000);
     expect(timer.remainingTime).toBe(0.5 * 60);
     expect(onIncreaseCount).not.toHaveBeenCalled();
